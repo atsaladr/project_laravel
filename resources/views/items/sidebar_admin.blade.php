ee11653e28ceaeeeee2acb91dba134c133e18872
atsal adriansyah <atsal007.aa@gmail.com>
2022-04-26 21:57:05 +0700

    penambahan sidebar admin

--- resources/views/items/sidebar_admin.blade.php
+++ resources/views/items/sidebar_admin.blade.php
@@ -97,8 +97,7 @@
 				</li><li>
 					<a href='?module=JS Arrays'><i class='fa fa-angle-double-right'></i>JS Arrays[ GALIH ADI PRATAMA]</a>
 				</li><li>
-					<a href='?module=JS Array Methods'><i class='fa fa-angle-double-right'>	   </i>JS Array Methods[ 		]</a>
-				</li><li>
+				<a href="{{ Route('jsatsal') }}"><i class='fa fa-angle-double-right'>	   </i>JS Atsal Adriansyah[ATSAL ADRIANSYAH]</a>				</li><li>
 					<a href='?module=JS Array Sort'><i class='fa fa-angle-double-right'>		  </i>JS Array Sort[	]</a>
 				</li><li>
 					<a href='?module=JS Array Iteration'><i class='fa fa-angle-double-right'></i>JS Array Iteration[    ]</a>
